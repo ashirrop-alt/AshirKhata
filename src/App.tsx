2387d1f39ef1d590f688310a95e97d6cff00cd83
Ali Ahmed <ashirrop@gmail.com>
2026-05-13 14:20:42 +0500

    SAsaf2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { supabase } from './lib/supabase';
 import { Toaster } from "sonner";
 import ActivityLog from './pages/ActivityLog';
-import { ThemeProvider } from "./components/theme-provider"; 
+import { ThemeProvider } from "./components/theme-provider";
+import PublicCustomerView from './components/PublicCustomerView';
 
 import Login from './pages/auth/Login';
 import Signup from './pages/auth/Signup';
@@ -40,12 +41,19 @@ function App() {
         />
 
         <Routes>
+          {/* 1. Ye hai wo PUBLIC route jo bina login ke khulega */}
+          <Route path="/view/:shareId" element={<PublicCustomerView />} />
+
+          {/* 2. Auth Routes */}
           <Route path="/login" element={!session ? <Login /> : <Navigate to="/" />} />
           <Route path="/signup" element={!session ? <Signup /> : <Navigate to="/" />} />
+
+          {/* 3. Protected Routes (Jo sirf login ke baad khulein) */}
           <Route path="/" element={session ? <Index /> : <Navigate to="/login" />} />
           <Route path="/customer/:id" element={session ? <Index /> : <Navigate to="/login" />} />
-          <Route path="*" element={<Navigate to="/" />} />
           <Route path="/activity" element={session ? <ActivityLog /> : <Navigate to="/login" />} />
+
+          {/* 4. Catch-all (Ye hamesha AKHIR mein honi chahiye) */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
